bbce8de52a59d74b1fb8bcfb2f5a1efc846179d4
tanmayahere <74092895+tanmayahere@users.noreply.github.com>
2023-03-07 23:09:59 +0530

    Update streamlit_app.py
    pandas import

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.text('🌮Kale, Spinach')
 streamlit.text('🥚Hard-Boiled Free-Egg')
 
 streamlit.header('🍒🍑Build your own Fruit breakfast Menu🍌🍍')
+
+impot pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
