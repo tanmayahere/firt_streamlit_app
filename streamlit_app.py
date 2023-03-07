f059e2fa03d0502cf5f463824e5f0ff4f68ee93f
tanmayahere <74092895+tanmayahere@users.noreply.github.com>
2023-03-07 22:48:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new Healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 & Blueberry')
+streamlit.text('Kale, Spinach')
+streamlit.text('Hard-Boiled Free-Egg')
