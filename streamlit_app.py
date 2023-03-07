9b829c53c52d22e1e24a8f40af557b82c499770e
tanmayahere <74092895+tanmayahere@users.noreply.github.com>
2023-03-07 22:52:17 +0530

    Update streamlit_app.py
    emoji added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents new Healthy diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 & Blueberry')
-streamlit.text('Kale, Spinach')
-streamlit.text('Hard-Boiled Free-Egg')
+streamlit.header('🍞Breakfast Menu')
+streamlit.text('🍇Omega3 & Blueberry')
+streamlit.text('🌮Kale, Spinach')
+streamlit.text('🥚Hard-Boiled Free-Egg')
